4b962e75fc7ed603df56c8f8e775dc717282ac13
Florian RICHER <florian.richer@unova.fr>
2021-06-30 08:45:11 +0200

    Add startify + autopair

--- configs/nvim/lua/plugins/init.lua
+++ configs/nvim/lua/plugins/init.lua
@@ -2,20 +2,22 @@ function configure_packages()
     call_with_helpers(require('plugins.lspconfig').init)
     call_with_helpers(require('plugins.compe').init)
     call_with_helpers(require('plugins.nvim-treesitter').init)
-    call_with_helpers(require('plugins.web-devicons').init)
+    call_with_helpers(require('plugins.nvim-web-devicons').init)
     call_with_helpers(require('plugins.nvim-comment').init)
+    call_with_helpers(require('plugins.nvim-autopairs').init)
 end
 
 function install_packages()
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
         
-    use {'dracula/vim'}
-    use {'whatyouhide/vim-gotham'}
-    use {'neovim/nvim-lspconfig'}
-    use {'kabouzeid/nvim-lspinstall'}
-    use {'hrsh7th/nvim-compe'}
-    use {'shaunsingh/moonlight.nvim'}
+    use 'dracula/vim'
+    use 'whatyouhide/vim-gotham'
+    use 'neovim/nvim-lspconfig'
+    use 'kabouzeid/nvim-lspinstall'
+    use 'hrsh7th/nvim-compe'
+    use 'shaunsingh/moonlight.nvim'
+    use 'windwp/nvim-autopairs'
 
     -- Telescope project search
     use {
@@ -23,12 +25,13 @@ function install_packages()
         requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
     }
 
-    use {'nvim-treesitter/nvim-treesitter'}
+    use 'nvim-treesitter/nvim-treesitter'
 
-    use 'kyazdani42/nvim-web-devicons' -- removing this line doesn't change the error
+    use 'kyazdani42/nvim-web-devicons'  -- removing this line doesn't change the error
     use 'kyazdani42/nvim-tree.lua'
     use 'kdheepak/lazygit.nvim'
     use 'terrortylor/nvim-comment'
+    use 'mhinz/vim-startify'            -- The fancy start screen for Vim.
 end
 
 function init()

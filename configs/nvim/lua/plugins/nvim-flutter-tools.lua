7acf33fbb65b32c8e7a2c4f93dfe3c3d341090be
Florian RICHER <florian.richer@unova.fr>
2022-01-18 21:31:12 +0100

    [NEOVIM] Flutter tools conf update (FVM + statusline)

--- configs/nvim/lua/plugins/nvim-flutter-tools.lua
+++ configs/nvim/lua/plugins/nvim-flutter-tools.lua
@@ -1,5 +1,13 @@
 local function init()
-  require('flutter-tools').setup()
+  require('flutter-tools').setup {
+    decorations = {
+      statusline = {
+        app_version = true,
+        device = true
+      }
+    },
+    fvm = true
+  }
 end
 
 return {

4e7b54b2df1c71c6b0f1eb180755e19799a340f0
Florian RICHER <florian.richer@unova.fr>
2022-01-18 21:19:06 +0100

    [NEOVIM] Remove dartls configuration (use flutter-tools instead)

--- configs/nvim/lua/plugins/lspconfig/dart.lua
+++
@@ -1,7 +0,0 @@
-local function init()
-  require'lspconfig'.dartls.setup{}
-end
-
-return {
-  init = init
-}
--- configs/nvim/lua/plugins/lspconfig/init.lua
+++ configs/nvim/lua/plugins/lspconfig/init.lua
@@ -4,7 +4,6 @@ local function init()
     call_with_helpers(require('plugins.lspconfig.typescript').init)
     call_with_helpers(require('plugins.lspconfig.ruby').init)
     call_with_helpers(require('plugins.lspconfig.php').init)
-    call_with_helpers(require('plugins.lspconfig.dart').init)
 end
 
 return {

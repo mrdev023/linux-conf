e6372f3f3aa7853932fb7ef856acda5ee803abd7
Florian RICHER <florian.richer@unova.fr>
2022-01-21 22:16:21 +0100

    [TMUX] Install when is already installed

--- scripts/tools/tmux/install.sh
+++ scripts/tools/tmux/install.sh
@@ -2,11 +2,11 @@
 source ./scripts/common.sh
 
 if [[ "$WITH_TMUX_CONFIG" == 'yes' || "$WITH_TMUX_CONFIG" == 'force' ]]; then
-  if [[ -d "$HOME/.tmux.conf" && "$WITH_TMUX_CONFIG" == 'force' ]]; then
+  if [[ -f "$HOME/.tmux.conf" && "$WITH_TMUX_CONFIG" == 'force' ]]; then
     rm -rf "$HOME/.tmux.conf"
     echo '[INFO] Tmux config removed | Forced by environment variable'
   fi
-  if [[ ! -d "$HOME/.tmux.conf" ]]; then
+  if [[ ! -f "$HOME/.tmux.conf" ]]; then
     ln -s "$CWD/configs/tmux/tmux.conf" "$HOME/.tmux.conf"
     echo "[INFO] Tmux config linked with symbolic link : $CWD/configs/tmux/tmux.conf -> $HOME/.tmux.conf"
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
